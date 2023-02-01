6ffc9fcb71f4d5a49c431bffaf989e5f62008253
DAg2no <dualwinder777.2.0@gmail.com>
2023-02-01 14:52:16 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue'
+import blogPost from './components/BlogPost.vue'
+
+</script>
+
+<template>
+    <div class="container pt-4">
+        <h1>APP</h1>
+<ButtonCounter />
+<ButtonCounter/>
+
+<blogPost title="Post 1"/>
+<blogPost title="Post 2"/>
+<blogPost title="Post 3"/>
+
+    </div>
+
+</template>
+
---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+
+let counter = ref(0)
+let increment = () => counter.value ++
+
+
+</script>
+
+<template>
+    <button @click="increment">counter {{ counter }}</button>
+</template>
